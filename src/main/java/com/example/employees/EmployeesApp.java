89c4effdcf9cd8777665545df10d91ff0a0cdda4
Majorka93 <Zaikina1993>
2022-01-31 15:56:07 +0300

    initial commit

--- src/main/java/com/example/employees/EmployeesApp.java
+++ src/main/java/com/example/employees/EmployeesApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestTaskApp {
+public class EmployeesApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestTaskApp.class, args);
+        SpringApplication.run(EmployeesApp.class, args);
     }
 
 }
